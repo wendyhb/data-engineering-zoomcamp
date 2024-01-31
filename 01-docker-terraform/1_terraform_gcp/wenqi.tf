d6c97755e843c6ea2500f9effc84d195844b1b80
Wendyhb <wendyworks@gmail.com>
2024-01-31 17:15:12 -0500

    Init commit

---
+++ 01-docker-terraform/1_terraform_gcp/wenqi.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.14.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "my-project-id"
+  region  = "us-central1"
+}
